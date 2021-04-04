5010a1e44569f888b22e5b7574a814afc1ec204c
Kevin Johnson <kevin.johnson_12@hotmail.com>
2021-04-04 17:09:02 -0500

    Finalized Assignment

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,5 +1,92 @@
 /* Add your Application JavaScript */
+const NewsList={
+  // some options
+  name: 'news-list',
+  template: `
+<div class="form-inline d-flex justify-content-center">
+    <div class="form-group mx-sm-3 mb-2">
+        <label class="sr-only" for="search">Search</label>
+        <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+        <button class="btn btn-primary mb-2" @click="searchNews">Search</button>
+    </div>
+</div>
+<div class="news">
+    <ul class="news__list">
+      <div class = "row">
+        <div class = "col-md-4">
+              <div class = "card">
+                <li v-for="article in articles" class="card">
+                  <h5 class = "card-title"> {{ article.title }}</h5>
+                  <img v-bind:src="article.urlToImage" id = 'img-new'>
+                  <p class = "card-text"> {{ article.description }}</p>
+                </li>
+              </div>
+            </div>
+        </div>  
+      </div>
+    </ul> 
+</div> 
+    `,
+    created() {
+      let self = this;
+      fetch('https://newsapi.org/v2/top-headlines?country=us',
+{
+headers: {
+      'Authorization': 'Bearer 976d9d9bfd784d2ab3679baf9d35ab04'
+  }
+})
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+    self.articles = data.articles;
+});
+    },
+    data() {
+        return {
+            articles: [],
+            searchTerm: ''
+        }
+},
+methods: {
+  searchNews() {
+    let self = this;
+    fetch('https://newsapi.org/v2/everything?q='+
+self.searchTerm + '&language=en', {
+headers: {
+    'Authorization': 'Bearer 976d9d9bfd784d2ab3679baf9d35ab04'
+} 
+})
+.then(function(response) {
+return response.json();
+})
+.then(function(data) {
+console.log(data);
+self.articles = data.articles;
+});
+} 
+}
+};
+
+const Home = {
+  name: 'Home',
+  template: `
+      <div class="home">
+       <img src="/static/images/logo.png" alt="VueJS Logo">
+       <h1>{{ welcome }}</h1>
+      </div> `,
+data() {
+  return {
+    welcome: 'Hello World! Welcome to VueJS'
+} }
+};
+
 const app = Vue.createApp({
+  components: {
+    'home': Home,
+    'news-list': NewsList
+  },
   data() {
     return {
       welcome: 'Hello World! Welcome to VueJS'
@@ -20,10 +107,10 @@ app.component('app-header', {
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                  <router-link to="/" class="nav-link">Home</router-link>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">News</a>
+                  <router-link to="/news" class="nav-link">News</router-link>
                 </li>
               </ul>
             </div>
@@ -51,4 +138,13 @@ app.component('app-footer', {
   }
 })
 
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHistory(),
+  routes: [
+      { path: '/', component: Home },
+      { path: '/news', component: NewsList }
+  ]
+});
+
+app.use(router)
 app.mount('#app');
